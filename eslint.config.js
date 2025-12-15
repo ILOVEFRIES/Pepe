16ea5f6639df3b9f9ab759a0e02afa5ad1cf9662
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 01:08:05 +0700

    refactor: Update ESLint configuration to remove TypeScript parser and plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,19 @@
+// eslint.config.js
 import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
   eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["src/**/*.ts"],
     languageOptions: {
-      parser: "@typescript-eslint/parser", // use TS parser
       ecmaVersion: "latest",
       sourceType: "module",
-    },
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
     },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "off",
-
-      // **Warn only for any**
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
