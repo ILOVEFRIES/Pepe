062f1995addc3cad334c2242ea67f0191da3702a
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 00:47:57 +0700

    refactor: Simplify removeColumnPrefix function and improve code readability

--- src/util/removeColumnPrefix.ts
+++ src/util/removeColumnPrefix.ts
@@ -1,55 +1,35 @@
-/// This function removes the column prefix in database query results
-///
-/// For example, given this object
-/// {
-///   a_id: "123",
-///   a_name: "name"
-/// }
-/// the function will transform it into this object
-/// {
-///   id: "123",
-///   name: "name"
-/// }
-
-// Excluded fields
 const excludeFields = [
   "item_total_price",
   "with_charges",
   "without_charges",
   "is_revoked",
-];
+] as const;
 
-const removeColumnPrefix = (obj: unknown): unknown => {
+export function removeColumnPrefix(obj: unknown): unknown {
   if (Array.isArray(obj)) {
-    // If the input is an array, recursively process each element
-    return obj.map((item) => removeColumnPrefix(item));
-  } else if (
-    typeof obj === "object" &&
-    obj !== null &&
-    !(obj instanceof Date)
-  ) {
-    // If the input is an object and not a Date, process each key-value pair
+    return obj.map(removeColumnPrefix);
+  }
+
+  if (typeof obj === "object" && obj !== null && !(obj instanceof Date)) {
     const newObj: Record<string, unknown> = {};
 
-    for (const key in obj as Record<string, unknown>) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const underscoreIndex = key.indexOf("_");
-        const newKey = excludeFields.includes(key)
-          ? key
-          : underscoreIndex !== -1
-          ? key.slice(underscoreIndex + 1)
-          : key;
+    for (const key in obj) {
+      if (!Object.prototype.hasOwnProperty.call(obj, key)) continue;
 
-        const value = (obj as Record<string, unknown>)[key];
-        newObj[newKey] = removeColumnPrefix(value);
-      }
+      const underscoreIndex = key.indexOf("_");
+      const newKey = excludeFields.includes(key as any)
+        ? key
+        : underscoreIndex !== -1
+        ? key.slice(underscoreIndex + 1)
+        : key;
+
+      newObj[newKey] = removeColumnPrefix(
+        (obj as Record<string, unknown>)[key]
+      );
     }
 
     return newObj;
-  } else {
-    // If the input is neither an array nor an object, or if it's a Date, return it as is
-    return obj;
   }
-};
 
-export default removeColumnPrefix;
+  return obj;
+}
