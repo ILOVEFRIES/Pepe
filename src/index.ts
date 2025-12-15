e9bf165da8717b50d6700bb94173a29625dcbac0
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 01:25:16 +0700

    feat: Add outlet menu routes to the main application

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { userRoutes } from "./routes/userRoute";
 import { outletRoutes } from "./routes/outletRoute";
 import { menuRoutes } from "./routes/menuRoute";
 import { menuSubitemRoutes } from "./routes/menuSubitemRoute";
+import { outletMenuRoutes } from "./routes/outletMenuRoute";
 import { logger } from "@bogeychan/elysia-logger";
 import cors from "@elysiajs/cors";
 
@@ -17,6 +18,7 @@ const app = new Elysia()
   .group("/outlets", outletRoutes)
   .group("/menus", menuRoutes)
   .group("/menu-subitems", menuSubitemRoutes)
+  .group("/outlet-menus", outletMenuRoutes)
   .listen(process.env.PORT || 9000);
 
 console.log(`Pepe running at ${app.server?.hostname}:${app.server?.port}`);
