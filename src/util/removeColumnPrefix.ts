a43b806d7eeecaef39a0c98b1872387541f2e732
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 01:27:18 +0700

    chore: Fixed issues with remove column prefix

--- src/util/removeColumnPrefix.ts
+++ src/util/removeColumnPrefix.ts
@@ -5,7 +5,13 @@ const excludeFields = [
   "is_revoked",
 ] as const;
 
-export function removeColumnPrefix(obj: unknown): unknown {
+type Primitive = string | number | boolean | null | undefined | Date;
+
+/**
+ * Removes column prefixes (e.g. o_id → id) recursively
+ * while preserving object shape.
+ */
+export function removeColumnPrefix<T>(obj: T): any {
   if (Array.isArray(obj)) {
     return obj.map(removeColumnPrefix);
   }
@@ -13,10 +19,11 @@ export function removeColumnPrefix(obj: unknown): unknown {
   if (typeof obj === "object" && obj !== null && !(obj instanceof Date)) {
     const newObj: Record<string, unknown> = {};
 
-    for (const key in obj) {
+    for (const key in obj as Record<string, unknown>) {
       if (!Object.prototype.hasOwnProperty.call(obj, key)) continue;
 
       const underscoreIndex = key.indexOf("_");
+
       const newKey = excludeFields.includes(key as any)
         ? key
         : underscoreIndex !== -1
