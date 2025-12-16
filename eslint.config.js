d2e11001526c650e6a8c83788be86333f52fa6f1
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 08:44:32 +0700

    chore: REmove extends

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,38 @@
 // eslint.config.js
 import eslint from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import securityPlugin from "eslint-plugin-security";
 
 export default [
-  // ESLint recommended rules (JS)
+  // ESLint recommended
   eslint.configs.recommended,
 
-  // TypeScript recommended rules
+  // TypeScript plugin recommended rules
   {
+    files: ["src/**/*.ts"],
     plugins: {
       "@typescript-eslint": tsPlugin,
     },
-    extends: ["plugin:@typescript-eslint/recommended"],
-    files: ["src/**/*.ts"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       "no-unused-vars": "warn",
       "no-undef": "off",
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 
-  // Security rules
+  // Security plugin recommended rules
   {
-    plugins: {
-      security: await import("eslint-plugin-security"),
-    },
-    extends: ["plugin:security/recommended"],
     files: ["src/**/*.ts"],
+    plugins: {
+      security: securityPlugin,
+    },
     rules: {
-      "security/detect-object-injection": "warn",
-      "security/detect-non-literal-fs-filename": "warn",
-      "security/detect-non-literal-regexp": "warn",
-      "security/detect-eval-with-expression": "warn",
+      ...securityPlugin.configs.recommended.rules,
     },
   },
 ];
