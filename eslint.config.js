30d688928a9e56ef2c55efa89fe52001588680ec
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 08:50:42 +0700

    chore: Turned off any type warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,35 @@
 // eslint.config.js
 import eslint from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import securityPlugin from "eslint-plugin-security";
 
 export default [
   // ESLint recommended
   eslint.configs.recommended,
 
-  // TypeScript plugin recommended rules
+  // TypeScript rules
   {
     files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
     plugins: {
       "@typescript-eslint": tsPlugin,
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
       "no-unused-vars": "warn",
       "no-undef": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 
-  // Security plugin recommended rules
+  // Security rules
   {
     files: ["src/**/*.ts"],
     plugins: {
