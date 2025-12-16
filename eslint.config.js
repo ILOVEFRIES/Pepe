9275094fcb7d97b79ace9c24d6b2ae76d70a76b0
Wilbert Sutardja <sutardjawilbert@gmail.com>
2025-12-16 08:43:16 +0700

    chore: made it cleaner

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,37 @@
+// eslint.config.js
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
+  // ESLint recommended rules (JS)
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+
+  // TypeScript recommended rules
   {
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    extends: ["plugin:@typescript-eslint/recommended"],
     files: ["src/**/*.ts"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
     },
-    plugins: {
-      security: require("eslint-plugin-security"),
-    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "off",
       "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+
+  // Security rules
+  {
+    plugins: {
+      security: await import("eslint-plugin-security"),
+    },
+    extends: ["plugin:security/recommended"],
+    files: ["src/**/*.ts"],
+    rules: {
       "security/detect-object-injection": "warn",
       "security/detect-non-literal-fs-filename": "warn",
       "security/detect-non-literal-regexp": "warn",
