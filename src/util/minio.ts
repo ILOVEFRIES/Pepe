47f038c70703800d377d443c869fc7c33a6b8029
Gregorius Ferdinand <ferdinand.gregorius27@gmail.com>
2026-05-11 14:14:39 +0700

    Chore(Minio): Fix import syntax in minio.ts

--- src/util/minio.ts
+++ src/util/minio.ts
@@ -1,9 +1,9 @@
 var Minio = require("minio");
 
 export const minioClient = new Minio.Client({
-  endPoint: process.env.S3_ENDPOINT!,
+  endPoint: process.env.S3_ENDPOINT,
   port: 9091,
   useSSL: true,
-  accessKey: process.env.S3_ACCESS_KEY_ID!,
-  secretKey: process.env.S3_SECRET_ACCESS_KEY!,
+  accessKey: process.env.S3_ACCESS_KEY_ID,
+  secretKey: process.env.S3_SECRET_ACCESS_KEY,
 });
